cbb04c8aea654e36763f07d1bef281d47f3d2aa2
suheyl41 <36995815+suheyl41@users.noreply.github.com>
2018-03-07 02:25:25 +0300

    Add files via upload

---
+++ odev3.py
@@ -0,0 +1,8 @@
+def adamBasiCiro():
+    a=int(input("satisMiktari:"))
+    b=int(input("birimSatisFiyati:"))
+    c=int(input("toplamCalisanSayisi:"))   
+    ciro=a*b
+    adamBasiCiro=ciro/c
+    print(adamBasiCiro)
+    
